69c4e3eb233227fa6fb7e3af88db510f756f241b
Tiziano Mele <tiziano.mele09@gmail.com>
2020-02-26 15:06:20 +0100

    Using cwd instead of absolute path

--- tests/test_filewriter.py
+++ tests/test_filewriter.py
@@ -1,11 +1,11 @@
 import unittest
 from file_writer import File_writer
-
+import os
 
 class basic_filewriter_unittest(unittest.TestCase):
 
 	def test_get_rand_line(self):
-		filepath = "/Users/tiz/Desktop/py-simple-loggen/test.txt"
+		filepath = os.getcwd() + "/tests/logtest.txt"
 		lines = ["p1\n", "p2\n", "p3\n", "p4\n"]
 
 		logger = File_writer(filepath, lines)
