4813766d9e45b889b9b4e12da46f051b63e375fb
Tiziano Mele <tiziano.mele09@gmail.com>
2020-02-26 15:27:14 +0100

    Added constructor test

--- tests/test_filewriter.py
+++ tests/test_filewriter.py
@@ -4,6 +4,14 @@ import os
 
 class basic_filewriter_unittest(unittest.TestCase):
 
+	def test_constructor(self):
+		filepath = os.getcwd() + "/tests/logtest.txt"
+		lines = ["p1\n", "p2\n", "p3\n", "p4\n"]
+
+		logger = File_writer(filepath, lines)
+		result = os.path.isfile(filepath)
+		self.assertTrue(result)
+
 	def test_get_rand_line(self):
 		filepath = os.getcwd() + "/tests/logtest.txt"
 		lines = ["p1\n", "p2\n", "p3\n", "p4\n"]
