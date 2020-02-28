40436ac3421484e5b29c1429b2e6a8ee63ef9251
Tiziano Mele <tiziano.mele09@gmail.com>
2020-02-28 16:45:51 +0100

    Added comments

--- tests/test_loggen.py
+++ tests/test_loggen.py
@@ -5,6 +5,9 @@ import os
 class basic_loggen_unittest(unittest.TestCase):
 
 	def get_file_no_lines(self, filepath):
+		"""
+		Calc number of lines in a file
+		"""
 		no_lines = 0
 		with open(filepath, 'r') as f:
 			for line in f:
@@ -13,7 +16,12 @@ class basic_loggen_unittest(unittest.TestCase):
 		return no_lines
 
 	def test_loggen_no_lines(self):
-		## input data
+		"""
+		Tests if after starting loggen thread the number of lines written
+		in file is what is expected
+		"""
+
+		# input data
 		thread_name = "Logger1"
 		filepath = os.getcwd() + "/tests/logtest.txt"
 		lines = ["p1", "p2", "p3", "p4"]
@@ -21,7 +29,7 @@ class basic_loggen_unittest(unittest.TestCase):
 		sleep_time = 1
 		no_total_lines = 22
 		prefix_no_lines = True
-		## ---
+		# ---
 
 		logger = Loggen(thread_name, filepath, lines, no_bulk_lines, sleep_time, no_total_lines, prefix_no_lines)
 		logger.start()
