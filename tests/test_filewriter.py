840bc26e855f6d1b83247ab46312ee4ad37ee193
Tiziano Mele <tiziano.mele09@gmail.com>
2020-02-28 15:15:46 +0100

    Now considering the default \n trailing & added bulk and single line unittests

--- tests/test_filewriter.py
+++ tests/test_filewriter.py
@@ -5,20 +5,61 @@ import os
 class basic_filewriter_unittest(unittest.TestCase):
 
 	def test_constructor(self):
+		## input data
 		filepath = os.getcwd() + "/tests/logtest.txt"
-		lines = ["p1\n", "p2\n", "p3\n", "p4\n"]
+		lines = ["p1", "p2", "p3", "p4"]
+		prefix_no_lines = True
+		## ---
 
-		logger = File_writer(filepath, lines)
+		logger = File_writer(filepath, lines, prefix_no_lines)
 		result = os.path.isfile(filepath)
 		self.assertTrue(result)
 
 	def test_get_rand_line(self):
+		## input data
 		filepath = os.getcwd() + "/tests/logtest.txt"
-		lines = ["p1\n", "p2\n", "p3\n", "p4\n"]
+		lines = ["p1", "p2", "p3", "p4"]
+		prefix_no_lines = True
+		## ---
 
-		logger = File_writer(filepath, lines)
+		logger = File_writer(filepath, lines, prefix_no_lines)
 		result = (logger.get_rand_line() in lines)
 		self.assertTrue(result)
+
+	def test_bulk_append_number_of_lines(self):
+		## input data
+		filepath = os.getcwd() + "/tests/logtest.txt"
+		lines = ["p1", "p2", "p3", "p4"]
+		prefix_no_lines = True
+		bulk_size = 10
+		## ---
+
+		logger = File_writer(filepath, lines, prefix_no_lines)
+		logger.append_bulk(bulk_size)
+
+		result = 0
+		with open(filepath, 'r') as f:
+			for line in f:
+				result += 1
+
+		self.assertEqual(result, bulk_size)
+
+	def test_single_append_number_of_lines(self):
+		## input data
+		filepath = os.getcwd() + "/tests/logtest.txt"
+		lines = ["p1", "p2", "p3", "p4"]
+		prefix_no_lines = True
+		## ---
+
+		logger = File_writer(filepath, lines, prefix_no_lines)
+		logger.append_single_line()
+
+		result = 0
+		with open(filepath, 'r') as f:
+			for line in f:
+				result += 1
+
+		self.assertEqual(result, 1)
 
 if __name__ == '__main__':
 	unittest.main()
