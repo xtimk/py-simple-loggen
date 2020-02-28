118341a7337d91d804843a3fdc91a23924d1e849
Tiziano Mele <tiziano.mele09@gmail.com>
2020-02-28 13:02:27 +0100

    Added principal loggen class

---
+++ loggen/__init__.py
@@ -0,0 +1,36 @@
+from threading import Thread
+from file_writer import File_writer
+import time
+
+class Loggen(Thread):
+	filepath = ""
+	source_lines = []
+
+	def __init__(self, thread_name, filepath, source_lines, no_bulk_lines, sleep_time, no_total_lines):
+		Thread.__init__(self)
+		self.name = thread_name
+		self.filepath = filepath
+		self.source_lines = source_lines
+		self.no_bulk_lines = no_bulk_lines
+		self.sleep_time = sleep_time
+		self.no_total_lines = no_total_lines
+
+		self.fw = File_writer(self.filepath, self.source_lines)
+
+
+	def run(self):
+		print("Thread <" + self.name + "> started.")
+		total_written_lines = 0
+		while total_written_lines < self.no_total_lines:
+			if (self.no_total_lines - total_written_lines >= self.no_bulk_lines):
+				self.fw.append_bulk(self.no_bulk_lines)
+				total_written_lines += self.no_bulk_lines
+				time.sleep(self.sleep_time)
+			else:
+				self.fw.append_bulk(self.no_total_lines - total_written_lines)
+				total_written_lines += self.no_total_lines - total_written_lines
+				time.sleep(self.sleep_time)
+		print("Thread <" + self.name + "> ended.")
+		print("Total written lines: " + str(total_written_lines))
+		print("Expected written lines: " + str(self.no_total_lines))
+
